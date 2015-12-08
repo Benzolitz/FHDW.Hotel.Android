8f17f228adebd700759c9e99ed8f1ddaab689a71
Lucas Engel <engellucas@gmx.de>
2015-12-08 09:50:50 +0100

    Pictures - renamed pictures - fixed picture view

--- app/src/main/java/fhdw/hotel/DomainModel/Booking.java
+++ app/src/main/java/fhdw/hotel/DomainModel/Booking.java
@@ -7,6 +7,8 @@ public class Booking {
     public int Id;
     public Date Arrival;
     public Date Departure;
+    public Guest Guest;
+    public Hotel Hotel;
     public ArrayList<Room> Rooms;
 
     // region Getter & Setter
@@ -29,6 +31,20 @@ public class Booking {
     }
     public void setDeparture(Date departure) {
         Departure = departure;
+    }
+
+    public fhdw.hotel.DomainModel.Guest getGuest() {
+        return Guest;
+    }
+    public void setGuest(fhdw.hotel.DomainModel.Guest guest) {
+        Guest = guest;
+    }
+
+    public fhdw.hotel.DomainModel.Hotel getHotel() {
+        return Hotel;
+    }
+    public void setHotel(fhdw.hotel.DomainModel.Hotel hotel) {
+        Hotel = hotel;
     }
 
     public ArrayList<Room> getRooms() {
